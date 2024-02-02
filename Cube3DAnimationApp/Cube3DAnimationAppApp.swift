5f072cbf5a6e07a2a2deea611fe15c25f7fbdf80
gokulggopi28 <gokulggopi28@gmail.com>
2024-02-02 10:13:25 +0530

    Initial Commit

---
+++ Cube3DAnimationApp/Cube3DAnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cube3DAnimationAppApp.swift
+//  Cube3DAnimationApp
+//
+//  Created by Gokul G Gopi on 02/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Cube3DAnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
